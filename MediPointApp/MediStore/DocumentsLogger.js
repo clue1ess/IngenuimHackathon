dc856d83aa403d25906b5ccf9e037242e9b67387
vaishnavipatil555 <vaishnavi10march2000@gmail.com>
2021-03-27 20:38:30 +0530

    Changes in DocumentsLogger

--- MediPointApp/MediStore/DocumentsLogger.js
+++ MediPointApp/MediStore/DocumentsLogger.js
@@ -1,21 +1,93 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react'
+import { Card, Button } from 'react-native-elements'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native'
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
 
+const list = [
+    {
+        file: 'file1',
+        size: 'size1'
+    },
+    {
+        file: 'file2',
+        size: 'size2'
+    },
+    {
+        file: 'file3',
+        size: 'size3'
+    },
+]
+export default class DocumentsLogger extends Component {
+    async openDocument(){
+        let result = await DocumentPicker.getDocumentAsync({});
+		alert(result.uri);
+        console.log(result);
+    }
+    pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.cancelled) {
+          setImage(result.uri);
+        }
+    };
+    handleDelete = (file) => {
+        alert(file);
+    }
+    handleSubmit = (file) => {
+        alert(file);
+    }
+    render(){
+        return(
+            <View>
+                {
+                    list.map((l, i)=>(
+                        <Card>
+                                <View style={{flexDirection: 'row'}}>
+                                    <View style={{flex: 5, justifyContent: 'center'}}>
+                                        <Text style={{marginBottom: 10}}>
+                                            File: { l.file }
+                                        </Text>
+                                        <Text style={{marginBottom: 10}}>
+                                            Size: { l.size }
+                                        </Text>
+                                    </View>
+                                    <View style={{flex: 1}}>
+                                        <Button
+                                            icon={{ type: 'font-awesome', name: 'trash', color: 'red', size: 15 }}
+                                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                                            type='clear'
+                                            containerStyle={{alignSelf: 'flex-end'}}
+                                            onPress={()=> this.handleDelete(l.file)}
+                                        />
+                                        <Button
+                                            icon={{ type: 'font-awesome', name: 'download', color: 'green', size: 15 }}
+                                            buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                                            type='clear'
+                                            containerStyle={{alignSelf: 'flex-end'}}
+                                            onPress={()=> this.handleSubmit(l.file)}
+                                        />
+                                    </View>
+                                </View>                       
+                                
+                        </Card>
 
-const DocumentsLogger = () => {
-    return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text>Documents Logger</Text>
-        </View>
-    );
+                    ))
+                }
+                <TouchableOpacity 
+                    onPress={()=>this.pickImage()}
+                    style={{marginTop: 50, marginBottom: 50, height: 50, justifyContent: 'center', width: '70%', alignItems: 'center', alignSelf: 'center', backgroundColor: 'green'}}>
+                    <Text style={{color: '#fff'}}>Upload Document</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
-
-
-
-export default DocumentsLogger;
